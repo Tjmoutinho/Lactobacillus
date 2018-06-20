912aa3dcc9abaf7f5c01661d5ee130d6ac47fa4b
tjmoutinho <tjm4k@virginia.edu>
2018-06-20 14:55:32 -0400

    probanno tests

--- Code/test_probanno_gapfill.py
+++ Code/test_probanno_gapfill.py
@@ -1,6 +1,10 @@
 import probanno
+import cobra
+import pickle
 
 reaction_probabilities = probanno.generate_reaction_probabilities('/scratch/tjm4k/Lactobacillus/fastas/1002365.5.faa', '/scratch/tjm4k/Lactobacillus/Data/GramPositive.json', genome_id='1002365.5')
-universal_model = cobra.io.read_json_model("/scratch/tjm4k/Lactobacillus/Data/GramPosUni.json")
+universal_model = cobra.io.load_json_model("/scratch/tjm4k/Lactobacillus/Data/GramPosUni.json")
 model = cobra.io.read_sbml_model('/scratch/tjm4k/Lactobacillus/gap_models/1002365.5.xml')
-rxn_list = probabilistic_gapfill(model, universal_model, reaction_probabilities, clean_exchange_rxns=True, default_penalties=None, dm_rxns=False, ex_rxns=False, **solver_parameters)
+rxn_list = probanno.probabilistic_gapfill(model, universal_model, reaction_probabilities)
+pickle.dump(rxn_list, open("/scratch/tjm4k/Lactobacillus/Data/probanno_test.list", "wb"))
+

9d8ffcb3534b1d8400a5de3c04268a29ec014ac6
Tom <tjm4k@virginia.edu>
2018-06-20 17:37:11 -0400

    Change to py script to rerun with new uni-model

--- Code/test_probanno_gapfill.py
+++ Code/test_probanno_gapfill.py
@@ -4,14 +4,14 @@ import pickle
 
 reaction_probabilities = probanno.generate_reaction_probabilities('/scratch/tjm4k/Lactobacillus/fastas/1002365.5.faa', '/scratch/tjm4k/Lactobacillus/Data/GramPosUni.json', genome_id='1002365.5')
 pickle.dump(reaction_probabilities, open("/scratch/tjm4k/Lactobacillus/Data/rxn_probs.dic", "wb"))
-# universal_model = cobra.io.load_json_model("/scratch/tjm4k/Lactobacillus/Data/GramPosUni.json")
-# model = cobra.io.read_sbml_model('/scratch/tjm4k/Lactobacillus/gap_models/1002365.5.xml')
+universal_model = cobra.io.load_json_model("/scratch/tjm4k/Lactobacillus/Data/GramPosUni.json")
+model = cobra.io.read_sbml_model('/scratch/tjm4k/Lactobacillus/gap_models/1002365.5.xml')
 
-# for i in range(100):
-# 	try:
-# 		rxn_list = probanno.probabilistic_gapfill(model, universal_model, reaction_probabilities)
-# 		pickle.dump(rxn_list, open("/scratch/tjm4k/Lactobacillus/Data/probanno_test.list", "wb"))
-# 		print("Passed after %s loops.") % (i)
-# 		break
-# 	except:
-# 		pass
+for i in range(100):
+	try:
+		rxn_list = probanno.probabilistic_gapfill(model, universal_model, reaction_probabilities)
+		pickle.dump(rxn_list, open("/scratch/tjm4k/Lactobacillus/Data/probanno_test.list", "wb"))
+		print("Passed after %s loops.") % (i)
+		break
+	except:
+		pass

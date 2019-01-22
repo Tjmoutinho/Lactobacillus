42397ee07d525bce7fb8cdf6eb71a98ae2c78632
tjmoutinho <tjm4k@virginia.edu>
2019-01-22 09:36:21 -0500

    Test Probannopy Likelihoods

--- Code/Create_likelihood_files.py
+++ Code/Create_likelihood_files.py
@@ -10,23 +10,26 @@ import glob
 # genome_ids = [x.replace("/scratch/tjm4k/Lactobacillus/models/","").replace(".xml","") for x in model_paths]
 
 # Read in additional bifido genomes
-count = 0
-with open('../Data/Bifido_genomes.csv') as csvfile:
-    genome_ids = []
-    for line in csvfile:
-        if count == 0:
-            count = 1
-            continue
-        genome_ids.append(line.strip().split(',')[0])
+# count = 0
+# with open('../Data/Bifido_genomes.csv') as csvfile:
+#     genome_ids = []
+#     for line in csvfile:
+#         if count == 0:
+#             count = 1
+#             continue
+#         genome_ids.append(line.strip().split(',')[0])
 
 # Existing Prob files
-existing_files = glob.glob('../likelihoods/*.probs')
-existing_files = [x.replace("../likelihoods/","").replace(".probs","") for x in existing_files]
+existing_files = glob.glob('../likelihoods_V4/*.probs')
+existing_files = [x.replace("../likelihoods_V4/","").replace(".probs","") for x in existing_files]
 
-for genome_id in genome_ids:
+genome_ids = pickle.load(open("../Data/Filtered_lacto_genome_ids.data", "rb"))
+
+for genome_id in genome_ids[0:2]:
 	if not genome_id in existing_files:
 		try:
-			reaction_probabilities = probanno.generate_reaction_probabilities('/scratch/tjm4k/Lactobacillus/fastas/'+ genome_id +'.faa', '/scratch/tjm4k/Lactobacillus/Data/GramPositive.json', genome_id = genome_id)
-			pickle.dump(reaction_probabilities, open('/scratch/tjm4k/Lactobacillus/likelihoods/'+ genome_id +'.probs', "wb"))
+# 			reaction_probabilities = probanno.generate_reaction_probabilities('/scratch/tjm4k/Lactobacillus/fastas/'+ genome_id +'.faa', '/scratch/tjm4k/Lactobacillus/Data/GramPositive.json', genome_id = genome_id)
+			reaction_probabilities = probanno.generate_reaction_probabilities('/scratch/tjm4k/Lactobacillus/fastas/'+ genome_id +'.faa', '/scratch/tjm4k/Lactobacillus/Data/universal_w_OK_rxns.json', genome_id = genome_id)
+			pickle.dump(reaction_probabilities, open('/scratch/tjm4k/Lactobacillus/likelihoods_V4/'+ genome_id +'.probs', "wb"))
 		except:
 			pass
